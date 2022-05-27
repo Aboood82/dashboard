415fc4ef156254574cd4a91df6e65dd5937b8316
Aboood82 <88140315+Aboood82@users.noreply.github.com>
2022-05-27 17:23:39 +0300

    Add files via upload

---
+++ Website/js/in.js
@@ -0,0 +1,5 @@
+const guild = document.getElementById("theg").innerHTML;
+const theg = document.getElementById(guild)
+// const sGuild = document.querySelector(guild)
+console.log(theg)
+theg.classList.add("active")
